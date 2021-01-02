e12eaead55e6d7379de75290645622df577ac0c9
hieuflynotes <hieu@flynotes.co.uk>
2021-01-02 04:26:42 +0000

    fx cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { Product } from "./model/Product";
 import { v4 as uuidv4 } from "uuid";
@@ -7,6 +8,9 @@ import { v4 as uuidv4 } from "uuid";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors()
+);
 
 // Start the server
 const port = Number(process.env.PORT || 5030);
@@ -20,62 +24,62 @@ let products: Product[] = [
     name: "Iphone X",
     price: 5000000,
     salePrice: 4000000,
-    image:"https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
+    image: "https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
   },
   {
     id: "1d27aff3-fcf9-42ef-8d50-0765a4438589",
     name: "Iphone X",
     price: 5000000,
     salePrice: 4000000,
-    image:"https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
+    image: "https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
   },
   {
     id: "96f90e8a-cc50-4702-a672-9bea47299c63",
     name: "Iphone X",
     price: 5000000,
     salePrice: 4000000,
-    image:"https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
+    image: "https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
   },
   {
     name: "Iphone X",
     price: 5000000,
     salePrice: 4000000,
-    image:"https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
+    image: "https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
     id: "4d5a9258-c622-4f06-98aa-28b91ddb06f8",
   },
   {
     name: "Iphone X",
     price: 5000000,
     salePrice: 4000000,
-    image:"https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
+    image: "https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
     id: "ac3f2704-a46f-47c1-a15c-b24ea5f76df0",
   },
   {
     name: "Iphone X",
     price: 5000000,
     salePrice: 4000000,
-    image:"https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
+    image: "https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
     id: "e01964d9-5f3c-4ad8-950d-bba52a322dac",
   },
   {
     name: "Iphone X",
     price: 5000000,
     salePrice: 4000000,
-    image:"https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
+    image: "https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
     id: "76cea652-4a73-400b-b4a7-6574bea076ed",
   },
   {
     name: "Iphone X",
     price: 5000000,
     salePrice: 4000000,
-    image:"https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
+    image: "https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
     id: "37c26d6c-2a78-4919-993f-e458759ccd7e",
   },
   {
     name: "Iphone X",
     price: 5000000,
     salePrice: 4000000,
-    image:"https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
+    image: "https://assets.swappie.com/iPhone-X-64GB-Silver-1-1-1-1.png",
     id: "a4575a22-30f9-407d-9552-c5c5b3727262",
   },
 ];
